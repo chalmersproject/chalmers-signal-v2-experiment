bbbaef279b13fa016e675cb3afefd05bd8031e47
Zach Donsky <zachary.donsky@gmail.com>
2019-01-30 21:14:14 -0500

    moved interrupt OUT OF main loop cause I'm dumb

--- works.py
+++ works.py
@@ -25,7 +25,8 @@ def buttonPressed(channel):
         print "pressed"
         EntranceCounter +=1
         sleep(0.1)
-
+#Add interrupt for GPIO pin
+GPIO.add_event_detect(22, GPIO.BOTH, callback=buttonPressed)
 
 
 #global variables
@@ -72,8 +73,7 @@ ReferenceFrame = None
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    #Add interrupt for GPIO pin
-    GPIO.add_event_detect(22, GPIO.BOTH, callback=buttonPressed)
+
 	# grab the raw NumPy array representing the image
     image = frame.array
     vs = frame.array
